94a7b4903c6730f715899bc9170a94ab318a3bf8
Jon Chambers <jon.chambers@gmail.com>
2022-12-03 00:15:58 -0500

    Add a solution for day 3, part 1

---
+++ src/bin/day03.rs
@@ -0,0 +1,81 @@
+use std::env;
+use std::error::Error;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let args: Vec<String> = env::args().collect();
+
+    if let Some(path) = args.get(1) {
+        let rucksacks: Vec<Rucksack> = BufReader::new(File::open(path)?)
+            .lines()
+            .filter_map(|line| line.ok())
+            .map(|line| Rucksack { items: line })
+            .collect();
+
+        let priority_sum: u32 = rucksacks.iter()
+            .filter_map(|rucksack| rucksack.find_misplaced_item())
+            .map(|item| Rucksack::priority(item))
+            .sum();
+
+        println!("Priority sum: {}", priority_sum);
+
+        Ok(())
+    } else {
+        Err("Usage: day03 INPUT_FILE_PATH".into())
+    }
+}
+
+struct Rucksack {
+    items: String,
+}
+
+impl Rucksack {
+    fn find_misplaced_item(&self) -> Option<char> {
+        let items: Vec<char> = self.items.chars().collect();
+
+        let first_compartment = &items[0..items.len() / 2];
+        let second_compartment = &items[(items.len() / 2)..];
+
+        for candidate in first_compartment {
+            if second_compartment.contains(candidate) {
+                return Some(*candidate)
+            }
+        }
+
+        None
+    }
+
+    fn priority(item: char) -> u32 {
+        match item {
+            'a'..='z' => item as u32 - 'a' as u32 + 1,
+            'A'..='Z' => item as u32 - 'A' as u32 + 27,
+            _ => panic!()
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_find_misplaced_item() {
+        assert_eq!(Some('p'), Rucksack { items: String::from("vJrwpWtwJgWrhcsFMMfFFhFp") }.find_misplaced_item());
+        assert_eq!(Some('L'), Rucksack { items: String::from("jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL") }.find_misplaced_item());
+        assert_eq!(Some('P'), Rucksack { items: String::from("PmmdzqPrVvPwwTWBwg") }.find_misplaced_item());
+        assert_eq!(Some('v'), Rucksack { items: String::from("wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn") }.find_misplaced_item());
+        assert_eq!(Some('t'), Rucksack { items: String::from("ttgJtRGJQctTZtZT") }.find_misplaced_item());
+        assert_eq!(Some('s'), Rucksack { items: String::from("CrZsJsPPZsGzwwsLwLmpwMDw") }.find_misplaced_item());
+    }
+
+    #[test]
+    fn test_priority() {
+        assert_eq!(16, Rucksack::priority('p'));
+        assert_eq!(38, Rucksack::priority('L'));
+        assert_eq!(42, Rucksack::priority('P'));
+        assert_eq!(22, Rucksack::priority('v'));
+        assert_eq!(20, Rucksack::priority('t'));
+        assert_eq!(19, Rucksack::priority('s'));
+    }
+}
