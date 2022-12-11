7ed9eb83a18a282f8236fff09244d1850e8a1155
Jon Chambers <jon.chambers@gmail.com>
2022-12-11 10:52:07 -0500

    Add a solution for day 11, part 1

---
+++ src/bin/day11.rs
@@ -0,0 +1,200 @@
+use std::error::Error;
+use std::fs;
+use std::str::FromStr;
+use itertools::Itertools;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let args: Vec<String> = std::env::args().collect();
+
+    if let Some(path) = args.get(1) {
+        let monkey_group = MonkeyGroup::from_str(fs::read_to_string(path)?.as_str())?;
+
+        println!("Monkey business after 20 rounds: {}", monkey_group.monkey_business(20));
+
+        Ok(())
+    } else {
+        Err("Usage: day11 INPUT_FILE_PATH".into())
+    }
+}
+
+struct MonkeyGroup {
+    monkeys: Vec<Monkey>,
+}
+
+impl FromStr for MonkeyGroup {
+    type Err = Box<dyn Error>;
+
+    fn from_str(string: &str) -> Result<Self, Self::Err> {
+        let mut monkeys = vec![];
+
+        for (empty, group) in &string.lines().group_by(|line| line.is_empty()) {
+            if !empty {
+                monkeys.push(Monkey::from_str(group.intersperse("\n").collect::<String>().as_str())?);
+            }
+        }
+
+        Ok(MonkeyGroup { monkeys })
+    }
+}
+
+impl MonkeyGroup {
+    fn monkey_business(mut self, rounds: usize) -> u32 {
+        let mut inspections = vec![0u32; self.monkeys.len()];
+
+        for _ in 0..rounds {
+            for m in 0..self.monkeys.len() {
+                let monkey = &self.monkeys[m];
+
+                let throws: Vec<(usize, u32)> = monkey.items
+                    .iter()
+                    .map(|item| {
+                        let worry_level = match monkey.operation {
+                            Operation::Add(addend) => item + addend,
+                            Operation::Multiply(multiplier) => item * multiplier,
+                            Operation::Square => item * item,
+                        } / 3;
+
+                        let destination = match worry_level % monkey.modulus == 0 {
+                            true => monkey.destinations[0],
+                            false => monkey.destinations[1],
+                        };
+
+                        (destination, worry_level)
+                    })
+                    .collect();
+
+                inspections[m] += throws.len() as u32;
+
+                throws.into_iter()
+                    .for_each(|(destination, worry_level)| self.monkeys[destination].items.push(worry_level));
+
+                self.monkeys[m].items.clear();
+            }
+        }
+
+        inspections.sort();
+        inspections[inspections.len() - 1] * inspections[inspections.len() - 2]
+    }
+}
+
+#[derive(Clone, Debug)]
+struct Monkey {
+    items: Vec<u32>,
+    operation: Operation,
+    modulus: u32,
+    destinations: [usize; 2],
+}
+
+impl FromStr for Monkey {
+    type Err = Box<dyn Error>;
+
+    fn from_str(string: &str) -> Result<Self, Self::Err> {
+        let mut lines = string.lines();
+
+        if let Some(monkey_id_line) = lines.next() {
+            if !monkey_id_line.starts_with("Monkey ") {
+                return Err("Bad monkey ID line".into());
+            }
+        } else {
+            return Err("No monkey ID line".into());
+        }
+
+        let items: Vec<u32> = if let Some(starting_items_line) = lines.next() {
+            if let ["  Starting items", worry_levels] = starting_items_line.split(": ").collect::<Vec<&str>>().as_slice() {
+                worry_levels.split(", ")
+                    .map(|worry_level| worry_level.parse())
+                    .collect::<Result<_, _>>()?
+
+            } else {
+                return Err("Could not parse starting items".into());
+            }
+        } else {
+            return Err("No starting items line".into());
+        };
+
+        let operation = if let Some(operation_line) = lines.next().and_then(|line| line.strip_prefix("  Operation: new = ")) {
+            match operation_line.split(' ').collect::<Vec<&str>>().as_slice() {
+                ["old", "+", addend] => Operation::Add(addend.parse()?),
+                ["old", "*", "old"] => Operation::Square,
+                ["old", "*", multiplier] => Operation::Multiply(multiplier.parse()?),
+                _ => return Err("Could not parse operation".into())
+            }
+        } else {
+            return Err("No operation line".into());
+        };
+
+        let modulus: u32 = lines
+            .next()
+            .and_then(|line| line.strip_prefix("  Test: divisible by "))
+            .ok_or("No test line")?
+            .parse()?;
+
+        let true_destination: usize = lines
+            .next()
+            .and_then(|line| line.strip_prefix("    If true: throw to monkey "))
+            .ok_or("No true destination line")?
+            .parse()?;
+
+        let false_destination: usize = lines
+            .next()
+            .and_then(|line| line.strip_prefix("    If false: throw to monkey "))
+            .ok_or("No false destination line")?
+            .parse()?;
+
+        Ok(Monkey {
+            items,
+            operation,
+            modulus,
+            destinations: [true_destination, false_destination],
+        })
+    }
+}
+
+#[derive(Copy, Clone, Debug)]
+enum Operation {
+    Add(u32),
+    Multiply(u32),
+    Square,
+}
+
+#[cfg(test)]
+mod test {
+    use indoc::indoc;
+    use super::*;
+
+    const TEST_MONKEYS: &str = indoc! {"
+        Monkey 0:
+          Starting items: 79, 98
+          Operation: new = old * 19
+          Test: divisible by 23
+            If true: throw to monkey 2
+            If false: throw to monkey 3
+
+        Monkey 1:
+          Starting items: 54, 65, 75, 74
+          Operation: new = old + 6
+          Test: divisible by 19
+            If true: throw to monkey 2
+            If false: throw to monkey 0
+
+        Monkey 2:
+          Starting items: 79, 60, 97
+          Operation: new = old * old
+          Test: divisible by 13
+            If true: throw to monkey 1
+            If false: throw to monkey 3
+
+        Monkey 3:
+          Starting items: 74
+          Operation: new = old + 3
+          Test: divisible by 17
+            If true: throw to monkey 0
+            If false: throw to monkey 1
+    "};
+
+    #[test]
+    fn test_monkey_business() {
+        let monkey_group = MonkeyGroup::from_str(TEST_MONKEYS).unwrap();
+        assert_eq!(10605, monkey_group.monkey_business(20));
+    }
+}
