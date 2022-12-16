6c6d46439e97f8fa4ad7f371612d114bb40e256f
Jon Chambers <jon.chambers@gmail.com>
2022-12-16 16:51:58 -0500

    Refactor: move caching logic into `travel_cost` itself

--- src/bin/day16.rs
+++ src/bin/day16.rs
@@ -11,7 +11,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = std::env::args().collect();
 
     if let Some(path) = args.get(1) {
-        let volcano = Volcano::from_str(fs::read_to_string(path)?.as_str())?;
+        let mut volcano = Volcano::from_str(fs::read_to_string(path)?.as_str())?;
 
         println!(
             "Maximum pressure released over 30 minutes: {}",
@@ -27,6 +27,8 @@ fn main() -> Result<(), Box<dyn Error>> {
 struct Volcano {
     flow_rates: HashMap<String, u32>,
     connections: HashMap<String, Vec<String>>,
+
+    travel_times: HashMap<(String, String), u32>,
 }
 
 impl FromStr for Volcano {
@@ -62,12 +64,13 @@ impl FromStr for Volcano {
         Ok(Volcano {
             flow_rates,
             connections,
+            travel_times: HashMap::new(),
         })
     }
 }
 
 impl Volcano {
-    fn maximum_pressure_release(&self, time_limit: u32) -> u32 {
+    fn maximum_pressure_release(&mut self, time_limit: u32) -> u32 {
         const START: &str = "AA";
 
         let mut unopened_valves: HashSet<String> = self
@@ -88,30 +91,23 @@ impl Volcano {
             exploration_stack.push(ExplorationAction::Explore(unopened_valve.clone()))
         });
 
-        let mut travel_cost_cache = HashMap::new();
         let mut path: Vec<(u32, String)> = vec![];
-
         let mut max_pressure_released = 0;
 
         while !exploration_stack.is_empty() {
             match exploration_stack.pop().unwrap() {
                 ExplorationAction::Explore(valve) => {
                     let current_time = path.last().map(|(time, _)| *time).unwrap_or(0);
+                    let previous_valve = path
+                        .last()
+                        .map_or_else(|| START, |(_, previous_valve)| previous_valve);
 
-                    let travel_time = {
-                        let previous_valve = path
-                            .last()
-                            .map_or_else(|| START, |(_, previous_valve)| previous_valve);
-
-                        *travel_cost_cache
-                            .entry((previous_valve.to_string(), valve.clone()))
-                            .or_insert_with(|| self.travel_cost(previous_valve, &valve))
-                    };
+                    let travel_time = self.travel_cost(previous_valve, &valve);
 
                     // Do we have enough time to reach and open the destination valve? We need a
                     // minute to open the valve, but if we finish opening it at the last instant,
                     // then it won't actually vent any pressure.
-                    if current_time + travel_time <= time_limit - 2 {
+                    if self.can_open_valve(previous_valve, &valve, current_time, time_limit) {
                         path.push((current_time + travel_time + 1, valve.clone()));
                         unopened_valves.remove(valve.as_str());
 
@@ -136,36 +132,44 @@ impl Volcano {
         max_pressure_released
     }
 
-    fn travel_cost(&self, start: &str, end: &str) -> u32 {
-        let mut tentative_distances = HashMap::from([(start.to_string(), 0)]);
-        let mut visited_valves: HashSet<String> = HashSet::new();
+    fn can_open_valve(&mut self, start: &str, valve: &str, current_time: u32, time_limit: u32) -> bool {
+        current_time + self.travel_cost(start, valve) <= time_limit - 2
+    }
 
-        while !visited_valves.contains(end) {
-            let (valve, distance) = tentative_distances
-                .iter()
-                .filter(|(valve, _)| !visited_valves.contains(*valve))
-                .min_by_key(|(_, distance)| *distance)
-                .unwrap();
+    fn travel_cost(&mut self, start: &str, end: &str) -> u32 {
+        *self.travel_times
+            .entry((start.to_string(), end.to_string()))
+            .or_insert_with(|| {
+                let mut tentative_distances = HashMap::from([(start.to_string(), 0)]);
+                let mut visited_valves: HashSet<String> = HashSet::new();
 
-            let valve = valve.clone();
-            let distance = *distance;
+                while !visited_valves.contains(end) {
+                    let (valve, distance) = tentative_distances
+                        .iter()
+                        .filter(|(valve, _)| !visited_valves.contains(*valve))
+                        .min_by_key(|(_, distance)| *distance)
+                        .unwrap();
 
-            for neighbor in self
-                .connections
-                .get(&valve)
-                .expect("Valve should have neighbors")
-            {
-                let neighbor_distance = tentative_distances
-                    .entry(neighbor.clone())
-                    .or_insert(u32::MAX);
+                    let valve = valve.clone();
+                    let distance = *distance;
 
-                *neighbor_distance = (*neighbor_distance).min(distance + 1);
-            }
+                    for neighbor in self
+                        .connections
+                        .get(&valve)
+                        .expect("Valve should have neighbors")
+                    {
+                        let neighbor_distance = tentative_distances
+                            .entry(neighbor.clone())
+                            .or_insert(u32::MAX);
 
-            visited_valves.insert(valve.clone());
-        }
+                        *neighbor_distance = (*neighbor_distance).min(distance + 1);
+                    }
 
-        *tentative_distances.get(end).unwrap()
+                    visited_valves.insert(valve.clone());
+                }
+
+                *tentative_distances.get(end).unwrap()
+            })
     }
 
     fn pressure_released(&self, sequence: &[(u32, String)], time_limit: u32) -> u32 {
@@ -216,7 +220,7 @@ mod test {
 
     #[test]
     fn test_travel_cost() {
-        let volcano = Volcano::from_str(TEST_VALVES).unwrap();
+        let mut volcano = Volcano::from_str(TEST_VALVES).unwrap();
 
         assert_eq!(0, volcano.travel_cost("AA", "AA"));
         assert_eq!(1, volcano.travel_cost("AA", "BB"));
@@ -225,7 +229,7 @@ mod test {
 
     #[test]
     fn test_maximum_pressure_release() {
-        let volcano = Volcano::from_str(TEST_VALVES).unwrap();
+        let mut volcano = Volcano::from_str(TEST_VALVES).unwrap();
 
         assert_eq!(1651, volcano.maximum_pressure_release(30));
     }
